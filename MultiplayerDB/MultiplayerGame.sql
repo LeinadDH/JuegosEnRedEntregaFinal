a0a58d58e4cbbb83372aaa7f11b89c2a382f6372
CryoStorage <dev.cryostorage@gmail.com>
2023-05-08 13:23:36 -0600

    carga inicial de datos

--- MultiplayerDB/MultiplayerGame.sql
+++ MultiplayerDB/MultiplayerGame.sql
@@ -1,41 +1,58 @@
+CREATE DATABASE dio
+
+USE dio
+
 CREATE TABLE Usuarios (
-  usuario_id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_usuario VARCHAR(50) NOT NULL,
-  correo_electronico VARCHAR(100) NOT NULL,
-  contraseña VARCHAR(50) NOT NULL,
-  nivel_de_experiencia INT NOT NULL
+  usuario_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  usuario_nombre  VARCHAR(50) UNIQUE NOT NULL ,
+  usuario_email  VARCHAR(100) UNIQUE NOT NULL,
+  usuario_contraseña VARCHAR(50) NOT NULL,
+  usuario_nivel INT NOT NULL
 );
 
-CREATE TABLE Tiempos (
-  tiempo_id INT AUTO_INCREMENT PRIMARY KEY,
-  usuario_id INT NOT NULL,
-  tiempo TIME NOT NULL,
-  fecha DATE NOT NULL,
-  FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id)
-);
+ALTER TABLE Usuarios ADD COLUMN usuario_password VARCHAR(50) NOT NULL;
+ALTER TABLE Usuarios DROP COLUMN usuario_contraseña;
 
+SELECT * FROM usuarios;
+
+CREATE TABLE Pistas(
+	pista_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	pista_nombre VARCHAR(255) NOT NULL,
+	pista_dificultad ENUM ('1','2','3','4','5') NOT NULL
+);
 
 CREATE TABLE Carreras (
-  carrera_id INT AUTO_INCREMENT PRIMARY KEY,
-  ubicacion VARCHAR(100) NOT NULL,
-  nivel_de_dificultad INT NOT NULL,
-  duracion_de_la_carrera TIME NOT NULL
+  carrera_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  pista_id INT UNSIGNED,
+  carrera_tiempo_fecha DATETIME NOT NULL,
+  carrera_duracion TIME NOT NULL,
+  FOREIGN KEY (pista_id) REFERENCES pistas(pista_id)
 );
 
 CREATE TABLE Clasificaciones (
-  clasificacion_id INT AUTO_INCREMENT PRIMARY KEY,
-  carrera_id INT NOT NULL,
-  usuario_id INT NOT NULL,
+  clasificacion_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  carrera_id INT UNSIGNED NOT NULL,
+  usuario_id INT UNSIGNED NOT NULL,
   tiempo TIME NOT NULL,
   fecha DATE NOT NULL,
   FOREIGN KEY (carrera_id) REFERENCES Carreras(carrera_id),
   FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id)
 );
 
-CREATE TABLE Usuarios_Carreras (
-  usuario_id INT NOT NULL,
-  carrera_id INT NOT NULL,
-  PRIMARY KEY (usuario_id, carrera_id),
-  FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id),
-  FOREIGN KEY (carrera_id) REFERENCES Carreras(carrera_id)
+CREATE TABLE carreras_x_usuario(
+	PRIMARY KEY (usuario_id ),
+  	usuario_id INT UNSIGNED NOT NULL,
+  	carrera_id INT UNSIGNED NOT NULL,
+  	FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id),
+  	FOREIGN KEY (carrera_id) REFERENCES Carreras(carrera_id)
 );
+
+CREATE TABLE usuarios_x_carrera(
+	PRIMARY KEY(carrera_id),
+	usuario_id INT UNSIGNED NOT NULL,
+	carrera_id INT UNSIGNED NOT NULL,
+	FOREIGN KEY (carrera_id) REFERENCES Carreras(carrera_id),
+	FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id)
+);
+
+SHOW TABLES
---
+++ MultiplayerDB/dioInitialLoading.sql
@@ -0,0 +1,22 @@
+USE dio
+
+INSERT INTO pistas(pista_nombre, pista_dificultad) VALUES
+	('downToBasics', 1),
+	('cityStreets', 2),
+	('flowingCurves',3),
+	('devilsPass',4);
+
+INSERT INTO Usuarios(usuario_nombre,usuario_email,usuario_password,usuario_nivel)
+VALUES 
+('johnsmith', 'johnsmith@gmail.com', 'password123', 1),
+('janedoe', 'janedoe@yahoo.com', 'secret321', 2),
+('michaelj', 'michaelj@outlook.com', 'mydog123', 1),
+('sthompson', 'sarahthompson@gmail.com', 'letmein456', 3),
+('davidl', 'davidl@protonmail.com', 'iamawesome', 2),
+('annar', 'annarodriguez@gmail.com', 'coolpassword', 1);
+
+INSERT INTO Carreras(pista_id, carrera_tiempo_fecha, carrera_duracion)
+VALUES 
+  (1, '2023-05-08 12:00:00', '01:30:00'),
+  (2, '2023-05-09 14:30:00', '02:15:00'),
+  (4, '2023-05-10 15:45:00', '01:50:00');
